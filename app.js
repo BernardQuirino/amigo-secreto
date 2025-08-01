278d7a3349d480bf344dc9dcec9aed69b56c7887
Bernard Quirino <bernardquirino@gmail.com>
2025-08-01 17:10:56 -0300

    Funcionar Adicionar nomes e alerta

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let guardarNomes = [];
+
+function limparCampo() {
+    document.getElementById('amigo').value = ''
+}
+
+function adicionarAmigo() {
+    let nomes = document.getElementById('amigo').value;
+
+    if (nomes == '') {
+        alert('Você precisa escrever um nome')
+    } else {
+        guardarNomes.push(nomes);
+        console.log(guardarNomes);
+    }
+    limparCampo();
+}
+
+
+
