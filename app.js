c39f3b8b5474e39c6f6588e82b70f0db08b7251f
Bernard Quirino <bernardquirino@gmail.com>
2025-08-08 18:42:47 -0300

    JS pronto + começo do README

--- app.js
+++ app.js
@@ -10,13 +10,44 @@ function adicionarAmigo() {
     let nomes = document.getElementById('amigo').value;
 
     if (nomes == '') {
-        alert('Você precisa escrever um nome')
+        alert('Você precisa escrever um nome');
     } else {
         guardarNomes.push(nomes);
         console.log(guardarNomes);
     }
     limparCampo();
+    atualizarLista();
 }
 
+function atualizarLista() {
+    let atualizar = document.getElementById('listaAmigos');
+    atualizar.innerHTML = '';
 
+    for (let i = 0; i < guardarNomes.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = guardarNomes[i];
+        atualizar.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if (guardarNomes.length === 0) {
+        alert('Você precisa adicionar ao menos um nome');
+        return;
+    }
+
+    let indice = Math.floor(Math.random() * guardarNomes.length);
+    let nomeSorteado = guardarNomes[indice];
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+
+    let visualizarSorteado = document.createElement('li');
+    visualizarSorteado.textContent = `O amigo secreto sorteado é: ${nomeSorteado}!`;
+
+    let atualizar = document.getElementById('listaAmigos');
+    atualizar.innerHTML = '';
+    
+    resultado.appendChild(visualizarSorteado);
+}
 
